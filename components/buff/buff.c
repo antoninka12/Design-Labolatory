6994337be90a2be5b78be359fc3f458eb24414fc
Patrycja <patrycja.pach@onet.pl>
2025-12-26 18:35:44 +0100

    buffor skończony

--- components/buff/buff.c
+++ components/buff/buff.c
@@ -1,24 +1,34 @@
 #include <stdint.h>
 #include "esp_timer.h" //aktualny czas w esp
 #include <stdbool.h> //do zmiennych typu bool
+#include "esp_log.h" 
 
 #include "buff.h"
+#include "oled_basic.h"
+#include "bitmap.h"
+
+#define I2C_PORT I2C_NUM_0 //do I2C z kodu Tosi
 
 en_buff buffer[BUFFER_SIZE]; //powołanie bufora
 uint8_t position=0; //pozycja w buforze
 
-//do przytrzymania 2s
+//save buff
 bool count=false; //czy już liczymy czas
+bool saved=false; //czy dana już zapisana
 uint64_t start=0;//czas startu
 uint64_t last_data=0; //ostatnie dane jakie dostał bufor
-en_buff last=EN_NONE; 
+en_buff last=EN_NONE;
+
+
+//read buff
+bool read=false;
 
 void clear_buff(){
     for(int i=0; i<BUFFER_SIZE; i++){
         buffer[i]=EN_NONE;
     }
     position=0;
-    count=false;
+    saved=false;
     start=0;
     last_data=0;
     last=EN_NONE;
@@ -26,54 +36,67 @@ void clear_buff(){
 
 void save_buff(en_buff current)
 {
-    uint64_t now = esp_timer_get_time() / 1000;//czas
-
-    //1. Nie włączony curr=0, last=0 
+    uint64_t now = esp_timer_get_time() / 1000;//czas 
+    
     if(current==EN_NONE){
-        last=EN_NONE;
-        count=false;
         start=0;
-    } else { //2. zgięcie curr=1 last=0
-        last_data = 0;
-        if(last==EN_NONE){
-            last=current;
-            start=now;
-            count=true;
-        }
+        saved=false;
+        //ESP_LOGI("BUFF", "pos=%u", position);
     }
 
-    //3.2. jeśli zmienił się stan przy liczeniu
-    if(current!=EN_NONE && last!=current){
-        last=current;
+    if(current!=EN_NONE && start==0){
         start=now;
-        count=true;
+        //ESP_LOGI("BUFF", "stan=%llu", start);
     }
 
-    //3.1. przytrzymanie przez 2s
-    if(count && (now-start>=2000)){
+    if(!saved && current!=EN_NONE && (now-start)>=4000){
         if(position<BUFFER_SIZE){
             buffer[position]=current;
             position++;
+            ESP_LOGI("BUFF", "Zapis[%d] = %d", position, current);
+            saved=true;
+            last_data=0;
         }
-        count=false; //i już nie licz
     }
 
     if(current==EN_NONE && position>0 && last_data==0){
         last_data=now;
+        ESP_LOGI("BUFF", "time");
     }
 
-    if (current==EN_NONE && position>0 && 
-        last_data!=0 && now-last_data>=5000){
-        //read_buff();
+    if(position>0 && (now-last_data>=3000) && current==EN_NONE){
+   
+        ESP_LOGI("BUFF", "read");
+        for(int i=0; i<position; i++){
+            send_buff(buffer[i]);
+            vTaskDelay(pdMS_TO_TICKS(1000));
+        }
         clear_buff();
+        
     }
-    
 } //void
 
 
-/*int read_buff(){
-    for(int i=0; i<position; i++){
-        return buffer[i];
+//wysyłanie po jednej rzeczy co sekundę
+void read_block_buff(){
+    read=true;
+
+    
+}
+
+void send_buff(en_buff x)
+{
+    switch(x){
+        case EN_JESC: 
+            ESP_ERROR_CHECK(oled_draw_bitmap(I2C_PORT, jesc_bitmap, jesc_bitmap_len)); break;
+        case EN_PIC:  
+            ESP_ERROR_CHECK(oled_draw_bitmap(I2C_PORT, pic_bitmap, pic_bitmap_len));  break;
+        case EN_NIE:   
+            ESP_ERROR_CHECK(oled_draw_bitmap(I2C_PORT, nie_bitmap, nie_bitmap_len));  break;
+        case EN_OK:   
+            ESP_ERROR_CHECK(oled_draw_bitmap(I2C_PORT, ok_bitmap, ok_bitmap_len));   break;
+        default:       
+            ESP_ERROR_CHECK(oled_clear(I2C_PORT)); break;
     }
-}*/
+}
 
--- components/buff/include/buff.h
+++ components/buff/include/buff.h
@@ -2,20 +2,22 @@
 #define BUFF_H
 
 #include <stdint.h>
+#include "driver/i2c.h"
+
 
 #define BUFFER_SIZE 32
 
 typedef enum {
     EN_NONE=0,
     EN_JESC,
-    EN_PIC
+    EN_PIC,
+    EN_NIE,
+    EN_OK
 } en_buff; 
 
-extern en_buff buffer[BUFFER_SIZE]; //powołanie bufora z c
-extern uint8_t position;
-
 void clear_buff(void);
-void save_value(en_buff val);
-
+void save_buff(en_buff current);
+void read_block_buff(void);
+void send_buff(en_buff x);
 
 #endif
